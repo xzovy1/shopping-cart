0448bf21c3e4dd34d0a11ce14a82a48b03362874
xzovyAtWork <jsteinca@jci.com>
2025-04-14 17:51:30 -0600

    fix: format Checkout ul

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -26,13 +26,15 @@ const Checkout = () => {
         <>
             <h1>Confirm your order</h1>
             <ul>
-                {cart.map(item => <li key={item.id}>
-                                        {item.title} x{item.quantity}
-                                        <p>
-                                            <button onClick={()=>changeQuantity(item, true)}>+</button>
-                                            <button onClick={()=>changeQuantity(item, false)}>-</button>
-                                        </p>
-                                    </li>)}
+                {cart.map(item => 
+                    <li key={item.id}>
+                        {item.title} x{item.quantity}
+                        <p>
+                            <button onClick={()=>changeQuantity(item, true)}>+</button>
+                            <button onClick={()=>changeQuantity(item, false)}>-</button>
+                        </p>
+                    </li>
+                )}
             </ul>
             <div>
                 Forget something?

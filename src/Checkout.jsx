86eedc058951663e1bbeec66a35ba1e66a64da37
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-04-17 13:09:55 -0600

    build: update order total on checkout page

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,48 +1,54 @@
 import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Checkout = () => {
     const location = useLocation();
-    // const cartItems = location.state?.cartItems || [];
     const [cartItems, setCartItems] = useState(location.state?.cartItems || [])
-    function changeQuantity(item, increment){
-        if(increment){
-            setCartItems(
-                
-                    cartItems.map(e => {
-                        if(e.id === item.id)return { ...e, quantity: e.quantity + 1}
-                        else{return e}
-                    })
-            )
-            }
-        else{
-            setCartItems(
-                cartItems.map(e => {
-                    if(e.id === item.id)return { ...e, quantity: e.quantity - 1}
-                    else{return e}
-                })
-                )
+    let handleTotalPrice = cartItems.reduce((accumulator, currentValue) => accumulator + (currentValue.price * currentValue.quantity), 0,);
+    const [totalPrice, setTotalPrice] = useState(handleTotalPrice);
+
+    useEffect(()=>{
+        setTotalPrice(handleTotalPrice)
+    },[handleTotalPrice])
+
+    const increment = (item) => {
+        setCartItems(
+            cartItems.map(e => {
+                if(e.id === item.id)return { ...e, quantity: e.quantity + 1}
+                else{return e}
+                        }))
         }
-    }
+    const decrement = (item) =>  {
+        if(item.quantity == 1){return setCartItems(cartItems.filter(e => e.id !== item.id))}
+        setCartItems(
+            cartItems.map(e => {
+                if(e.id === item.id && e.quantity !== 0)return { ...e, quantity: e.quantity - 1}
+                else{return e}
+                }
+        ))
+    }         
+    
     return (
         <div data-testid="checkout">
             <h1>Confirm your order</h1>
             <ul>
                 {cartItems.map(item => 
                     <li key={item.id}>
-                        {item.title} x{item.quantity}
-                        <p>
-                            <button onClick={()=>changeQuantity(item, true)}>+</button>
-                            <button onClick={()=>changeQuantity(item, false)}>-</button>
-                        </p>
+                        <span>{item.title}</span> <span data-testid="price">${item.price}</span> x <span data-testid="quantity">{item.quantity}</span>
+                        <div>
+                            <button onClick={()=>increment(item)}>+</button>
+                            <button onClick={()=>decrement(item)}>-</button>
+                        </div>
                     </li>
                 )}
             </ul>
+            <div data-testid="order-total">Total: ${totalPrice}</div>
             <div>
                 Forget something?
                 <Link to="/shopping-page" state={{cartItems}}> Return to Shopping Page</Link>
             </div>
                 {cartItems.length > 0 ? <Link to="/order-complete"><button>Place Order</button></Link> : ''}
+                
         </div>
     )
 }
--- tests/Checkout.test.jsx
+++ tests/Checkout.test.jsx
@@ -15,7 +15,7 @@ const dummyCart = {cartItems: [
   }
 ]}
 
-test("heading with cart should load",()=>{
+test("heading with empty cart should load",()=>{
   renderWithMemRouter()
   expect(screen.getByRole('heading', {name: "Confirm your order"})).toBeInTheDocument()
   expect(screen.getByRole('list')).toBeInTheDocument()
@@ -25,7 +25,60 @@ test("heading with cart of 1 item should load",()=>{
   renderWithMemRouter(dummyCart);
   expect(screen.getByRole('heading', {name: "Confirm your order"})).toBeInTheDocument()
   expect(screen.getByRole('list').childNodes.length).toBe(1)
+})
+
+test("clicking + and - button should change quantity by 1", async ()=>{
+  const {user} = renderWithMemRouter(dummyCart);
+  const increment = screen.getByRole("button", {name: "+"});
+  const decrement = screen.getByRole("button", {name: "-"}); 
+
+  await user.click(increment)
+  expect(screen.getByTestId("quantity").textContent).toBe("2")
+  await user.click(decrement)
+  expect(screen.getByTestId("quantity").textContent).toBe("1")
+
+})
+
+test("quantity should not go below 1", async () => {
+  const {user} = renderWithMemRouter(dummyCart);
+  const decrement = screen.getByRole("button", {name: "-"});
   
+  expect(screen.getByTestId("quantity").textContent).toBe("1");
+  await user.click(decrement)
+  expect(screen.queryByTestId("quantity")).not.toBeInTheDocument();
+})
+
+test("should be unable to place an order with an item that has a quantity of 0", async()=>{
+  const {user} = renderWithMemRouter({cartItems: [
+    {
+        category: "test category 1",
+        description: "test desc. 1",
+        id: 1,
+        image: 'testImg.url',
+        price: 11,
+        rating: {rate: 4.9, count: 120},
+        title: "test title 1",
+        quantity: 1
+    }
+  ]});
+  const decrement = screen.getByRole("button", {name: "-"});
+  await user.click(decrement)
+  expect(screen.queryByRole("button", {name: "Place Order"})).not.toBeInTheDocument();
+  expect(screen.queryByRole('heading', {name: "Order Placed. Thank you!"})).not.toBeInTheDocument();
+})
+
+
+test("order total should display total value of items in the cart", ()=>{
+  renderWithMemRouter(dummyCart);
+  const orderTotal = screen.getByTestId("order-total").textContent
+  expect(orderTotal).toBe("Total: $11")
+})
+
+test("Place order button should not be visible with no items in cart", async ()=>{
+  renderWithMemRouter()
+  expect(screen.getByRole('heading', {name: "Confirm your order"})).toBeInTheDocument()
+  expect(screen.getByRole('list').childNodes.length).toBe(0)
+  expect(screen.queryByText("Place Order")).not.toBeInTheDocument();
 })
 
 test("clicking checkout should go to Order complete page", async ()=>{
@@ -36,20 +89,11 @@ test("clicking checkout should go to Order complete page", async ()=>{
     await waitFor((()=>screen.getByRole('heading', {name: "Order Placed. Thank you!"})))
 })
 
-test("Place order button should not be visible with no items in cart", async ()=>{
-  renderWithMemRouter()
-  expect(screen.getByRole('heading', {name: "Confirm your order"})).toBeInTheDocument()
-  expect(screen.getByRole('list').childNodes.length).toBe(0)
-  expect(screen.queryByText("Place Order")).not.toBeInTheDocument();
-})
-
 test("clicking continue shopping should go to shopping page", async ()=>{
     const {user} = renderWithMemRouter(dummyCart)
     expect(screen.getByRole('heading', {name: "Confirm your order"})).toBeInTheDocument()
     expect(screen.getByRole('list').childNodes.length).toBe(1)
     await user.click(screen.getByRole("link", {name: 'Return to Shopping Page'}))
-      
     await waitFor((()=>expect(screen.getByTestId('entered-store')).toBeInTheDocument()))
-    screen.debug();
 })
 
